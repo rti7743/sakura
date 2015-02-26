0c7696e60a284a96dc794806df3d139b5ef9099d
mocaskr <mocaskr@users.noreply.github.com>
2015-02-26 22:12:43 +0900

    Imp: タブバーの項目幅の調整・iniで設定可能に
    [patchunicode:#980]
    skrw_imp_tab_width_v0_2.patch

--- sakura_core/config/system_constants.h
+++ sakura_core/config/system_constants.h
@@ -558,12 +558,15 @@
 	Version x175:
 	メインメニュー自動更新 2015.02.26 Moca
 
+	Version x176:
+	タブバーの最大・最小値設定 2014.07.04 Moca
+
 	-- 統合されたので元に戻す（1000～1023が使用済み） 	2008.11.16 nasukoji
 	-- Version 1000:
 	-- バージョン1000以降を本家統合までの間、使わせてください。かなり頻繁に構成が変更されると思われるので。by kobake 2008.03.02
 
 */
-#define N_SHAREDATA_VERSION		175
+#define N_SHAREDATA_VERSION		176
 #define STR_SHAREDATA_VERSION	NUM_TO_STR(N_SHAREDATA_VERSION)
 #define	GSTR_SHAREDATA	(_T("SakuraShareData") _T(CON_SKR_MACHINE_SUFFIX_) _T(_CODE_SUFFIX_) _T(_DEBUG_SUFFIX_) _T(STR_SHAREDATA_VERSION))
 
