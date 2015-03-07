b55b95ad4f2921991f017f8d3874b3526e962263
mocaskr <mocaskr@users.noreply.github.com>
2015-03-07 23:03:16 +0900

    Svn リビジョン表示コメントアウト

--- sakura/preBuild.bat
+++ sakura/preBuild.bat
@@ -4,12 +4,12 @@ HeaderMake -in=..\sakura_core\Funccode_x.hsrc -out=..\sakura_core\Funccode_defin
 HeaderMake -in=..\sakura_core\Funccode_x.hsrc -out=..\sakura_core\Funccode_enum.h -mode=enum -enum=EFunctionCode
 MakefileMake -file=..\sakura_core\Makefile -dir=..\sakura_core
 
-SubWCRev.exe "..\\" "..\sakura_core\svnrev_template.h" "..\sakura_core\svnrev.h"
-if %ERRORLEVEL% NEQ 0 (
-  echo Automatic revision update unavailable, using generic template instead.
-  echo You can safely ignore this message - see svnrev.h for details.
-  copy /Y "..\sakura_core\svnrev_unknown.h" "..\sakura_core\svnrev.h"
-)
+rem SubWCRev.exe "..\\" "..\sakura_core\svnrev_template.h" "..\sakura_core\svnrev.h"
+rem if %ERRORLEVEL% NEQ 0 (
+rem   echo Automatic revision update unavailable, using generic template instead.
+rem   echo You can safely ignore this message - see svnrev.h for details.
+rem   copy /Y "..\sakura_core\svnrev_unknown.h" "..\sakura_core\svnrev.h"
+rem )
 
 ENDLOCAL
 :: Always return an errorlevel of 0 -- this allows compilation to continue if SubWCRev failed.
